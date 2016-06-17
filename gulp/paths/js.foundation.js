ee5d5a081736409fc6dca60a13622d872969cd86
dshved <dshved@hotmail.com>
2016-06-18 00:24:04 +0300

    Исправлены проблемы с отображением svg иконок в IE 11

--- gulp/paths/js.foundation.js
+++ gulp/paths/js.foundation.js
@@ -2,5 +2,6 @@
 
 module.exports = [
 	'./bower_components/jquery/dist/jquery.min.js',
-	'./bower_components/highlightjs/highlight.pack.min.js'
+	'./bower_components/highlightjs/highlight.pack.min.js',
+	'./bower_components/svg4everybody/dist/svg4everybody.js'
 ];

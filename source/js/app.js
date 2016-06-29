ab107a3f27d535907655d3138e55f0816b9eee5e
Dmitry Shved <dshved@hotmail.com>
2016-06-29 14:06:28 +0300

    Валидация формы отправкаи сообщения

--- source/js/app.js
+++ source/js/app.js
@@ -29,7 +29,6 @@ var myModule = (function() {
   };
 
   var _backgroundClick = function(e) {
-    // e.preventDefault();
     if (e.target.className == 'wrapper bg') {
       $('.flip').removeClass('flipping');
       $('.login-btn').css({
@@ -57,11 +56,11 @@ myModule.init();
 
 $(document).ready(function() {
 
-  $('.tabs__control-link').on('click', function(e){
+  $('.tabs__control-link').on('click', function(e) {
     e.preventDefault();
     var item = $(this).closest('.tabs__controls-item'),
-        contentItem = $('.tabs__item'),
-        itemPosition = item.index();
+      contentItem = $('.tabs__item'),
+      itemPosition = item.index();
     contentItem.eq(itemPosition)
       .add(item)
       .addClass('active')
@@ -280,8 +279,31 @@ var validateForm = (function() {
   }
 
   function _setUpListners() {
-    $('#auth').submit(_Auth);
+    $('#auth').submit(_auth);
+    $('#feedback').submit(_sendMessage);
     $('#user_human').click(_check);
+    $('body').on('click', '.error__close', _closeMsg);
+  }
+
+  function _sendMessage(e) {
+    e.preventDefault();
+    var result = _validateMessageForm();
+
+    if (result === true) {
+      _errorMessage('Ваше сообщение успешно отправлено!');
+      document.getElementById("feedback").reset();
+    } else {
+      _errorMessage(result['message']);
+
+    }
+    
+  }
+
+
+  function _closeMsg(e) {
+    e.preventDefault();
+    $('.error').remove();
+    $('body').css('overflow', 'auto');
   }
 
   function _check() {
@@ -296,40 +318,83 @@ var validateForm = (function() {
 
   }
 
-  function _validateForm() {
-    var $this = $('#auth'),
-      user_human = $this.find('#user_human'),
-      human_yes = $this.find('#human_yes'),
-      valid = true;
-    if (!user_human.prop('checked')) {
-      valid = false;
-    }
-    if (!human_yes.prop('checked')) {
-      valid = false;
-    }
-    return valid;
+  function _errorMessage(message) {
+    var msg = '<div class="error">' +
+      '<div class="error__container">' +
+      '<a href="#" class="error__close">' +
+      '<i class="fa fa-times-circle" aria-hidden="true"></i></a>' +
+      '<div class="error__message">' + message + '</div></div></div>';
+    $('body').prepend(msg);
+    $('.error').css('top', $('body').scrollTop() + 'px');
+    $('body').css('overflow', 'hidden');
+  };
 
+  function isEmail(email) {
+    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    return regex.test(email);
+  }
+
+  function _validateMessageForm() {
+    var $this = $(this),
+      form = $('#feedback'),
+      user_name = form.find('#user_name'),
+      user_email = form.find('#user_email'),
+      msg_text = form.find('#msg_text'),
+      obj = {};
+    var validEmail  = isEmail(user_email.val());
+
+    if (user_name.val() === '' || user_email.val() === '' || msg_text.val() === '') {
+      obj.valid = false;
+      obj.message = "Вы заполнили не все поля!";
+      return obj;
+    }
+    if (!validEmail) {
+      obj.valid = false;
+      obj.message = "Введите корректный Email!";
+      return obj;
+    }
+
+    return true;
 
   }
 
-  function _Auth(e) {
-    e.preventDefault();
-    var form = $('#auth');
-    if (_validateForm()) {
-      location.href = 'admin.html';
-      // username = $("#user_login").val();
-      // password = $("#user_pass").val();
 
-      // $.ajax({
-      //   type: "POST",
-      //   url: "/login",
-      //   data: "name=" + username + "&pass=" + password,
-      //   success: function(html) {
-      //     console.log(data);
-      //   }
-      // });
+  function _validateAuthForm() {
+    var $this = $(this),
+      form = $('#auth'),
+      user_login = form.find('#user_login'),
+      user_pass = form.find('#user_pass'),
+      user_human = form.find('#user_human'),
+      human_yes = form.find('#human_yes'),
+      obj = {};
+
+    if (user_login.val() === '' || user_pass.val() === '') {
+      obj.valid = false;
+      obj.message = "Вы заполнили не все поля!";
+      return obj;
+    }
+
+    if (!user_human.prop('checked') || !human_yes.prop('checked')) {
+      obj.valid = false;
+      obj.message = "Роботам тут не место!";
+      return obj;
+    }
+
+    return true;
+
+  }
+
+  function _auth(e) {
+    e.preventDefault();
+    var form = $('#auth'),
+      result = _validateAuthForm();
+
+    if (result === true) {
+      location.href = 'admin.html';
+      document.getElementById("auth").reset();
     } else {
-      alert('Ошибка!');
+      _errorMessage(result['message']);
+
     }
   }
 
